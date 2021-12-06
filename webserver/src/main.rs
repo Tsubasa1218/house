434992cca8e88455a5edc58fea7515b34057362d
Juan Suaza <juan.suaza1218@gmail.com>
2021-12-06 18:36:54 -0500

    Add DB and connect to it from the server using Rocket and rusqlite

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,11 +1,34 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+
+use rocket_contrib::databases::rusqlite;
+
+#[database("house_db")]
+struct HouseDBConn(rusqlite::Connection);
+
+
+fn print_test(conn: &rusqlite::Connection) -> rusqlite::Result<Vec<String>> {
+    let mut stmt = conn.prepare("SELECT name FROM test")?;
+    let rows = stmt.query_map(&[], |row| row.get(0))?;
+
+    let mut names = Vec::new();
+        for name_result in rows {
+        names.push(name_result?);
+    }
+
+
+    Ok(names)
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello world"
+fn index(conn: HouseDBConn) -> rusqlite::Result<String> {
+    let result = print_test(&*conn)?;
+
+    Ok(result.join("-"))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().attach(HouseDBConn::fairing()).mount("/", routes![index]).launch();
+
 }
